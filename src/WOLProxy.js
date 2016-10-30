3f29962d302388828d02a0523477a5cfd7114e8b
Łukasz Kużyński 'wookieb <lukasz.kuzynski@gmail.com>
2016-10-30 16:58:30 +0100

    Initial commit

---
+++ src/WOLProxy.js
@@ -0,0 +1,154 @@
+const net = require('net');
+const tls = require('tls');
+const wol = require('wol');
+const debug = require('debug')('wol-proxy');
+
+class WOLProxy {
+
+    constructor() {
+        this._WOLSentDate = null;
+    }
+
+    /**
+     * @param {(number|object)} sourceDefinition port number or object
+     * @param {number} sourceDefinition.port
+     * @param {string} [sourceDefinition.interface='0.0.0.0']
+     */
+    source(sourceDefinition) {
+        this.source = sourceDefinition;
+        return this;
+    }
+
+    sourceSSL(sourceSSL) {
+        this.sourceSSLOptions = sourceSSL;
+        return this;
+    }
+
+    /**
+     *
+     * @param {Object} targetDefinition
+     * @param {string} [targetDefinition.hostname='localhost']
+     * @param {number} targetDefinition.port
+     * @param {string} targetDefinition.MAC MAC address to machine to wake up
+     */
+    target(targetDefinition) {
+        this.target = targetDefinition;
+        return this;
+    }
+
+    targetSSL(targetSSL) {
+        this.targetSSLOptions = targetSSL;
+        return this;
+    }
+
+
+    run() {
+        if (!this.source) {
+            throw new Error('Missing source configuration');
+        }
+
+        if (!this.target) {
+            throw new Error('Missing target configuration');
+        }
+
+        this._createServer();
+        this._createTargetSocket();
+    }
+
+    _createServer() {
+        const options = Object.assign({}, this.sourceSSLOptions || {}, {
+            pauseOnConnect: true
+        });
+
+        this.server = this.sourceSSLOptions ? tls.createServer(options) : net.createServer(options);
+        this.server.on('connection', (connection) => {
+
+            debug('New connection arrived');
+            const targetSocket = this._createTargetSocket();
+            targetSocket.on('connect', () => {
+                connection.pipe(targetSocket).pipe(connection);
+            });
+            this._connectToTarget(targetSocket);
+
+            connection.on('close', () => {
+                targetSocket.destroy();
+                connection.destroy();
+            });
+        });
+        this.server.listen(this.source.port, this.source.interface);
+    }
+
+    _createTargetSocket() {
+        const options = Object.assign({}, this.targetSSLOptions || {});
+        const targetSocket = (() => {
+            const basicSocket = new net.Socket();
+            if (!this.targetSSLOptions) {
+                return basicSocket;
+            }
+            return tls.TLSSocket(basicSocket, options);
+        })();
+
+        targetSocket.on('connect', () => {
+            debug('Connected to target endpoint');
+            targetSocket.isConnected = true;
+        });
+        targetSocket.on('close', () => {
+            targetSocket.isConnected = false;
+        });
+        targetSocket.on('error', (error) => {
+            debug('Target error', error);
+            switch (error.code) {
+                case 'EHOSTDOWN':
+                case 'ECONNREFUSED':
+                case 'ETIMEDOUT':
+                    if (this.hasWOLBeenSent) {
+                        this._connectToTarget(targetSocket);
+                    }
+                    break;
+
+                default:
+                    debug('Unhandled error', error);
+                    break;
+            }
+        });
+        return targetSocket;
+    }
+
+    _connectToTarget(socket) {
+        if (socket.isConnected || socket.connecting) {
+            return;
+        }
+
+        debug('Connection to target');
+        const options = Object.assign({}, this.targetSSLOptions || {});
+        socket.connect(this.target.port, this.target.host, options);
+
+        setTimeout(() => {
+            if (!socket.isConnected) {
+                this._wakeUpTarget();
+            }
+        }, 3000);
+    }
+
+    _wakeUpTarget() {
+        if (this.hasWOLBeenSent) {
+            return;
+        }
+
+        debug('sending WOL');
+        wol.wake(this.target.MAC, (err) => {
+            if (err) {
+                debug('Unabled to send WOL', err);
+                return;
+            }
+            debug('WOL sent');
+            this._WOLSentDate = new Date();
+        });
+    }
+
+    get hasWOLBeenSent() {
+        return this._WOLSentDate && (Date.now() - this._WOLSentDate.getTime()) < 600000;
+    }
+}
+
+module.exports = WOLProxy;
